dd4c056b1cd5e8417c678ba601002c2bb056075e
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2026-01-03 09:05:25 +0700

    feat(theme): perform the light/dark function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { Flex, Text, Button } from "@radix-ui/themes";
+import { Button } from "./components/ui/button";
+import { useTheme } from "./hooks/useTheme";
+import { Sun } from "lucide-react";
+import { MoonStar } from "lucide-react";
+import { MonitorCog } from "lucide-react";
 function App() {
+  const { theme, setTheme } = useTheme();
+  console.log(theme);
   return (
     <div>
-      <Flex direction="column" gap="2">
-        <Text>Hello from Radix Themes :)</Text>
-        <Button>Let's go</Button>
-      </Flex>
+      <Button asChild>
+        <a href="#!">Hello</a>
+      </Button>
+
+      <Button onClick={() => setTheme("light")}>
+        <Sun />
+      </Button>
+      <Button onClick={() => setTheme("dark")}>
+        <MoonStar />
+      </Button>
+      <Button onClick={() => setTheme("system")}>
+        <MonitorCog />
+      </Button>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import "./index.css";
 import App from "./App.jsx";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { ThemeProvider as ThemeProviderContext } from "./context/ThemeProvider";
 createRoot(document.getElementById("root")).render(
-  <Theme>
-    <App />
-  </Theme>
+  <ThemeProviderContext defaultTheme="system">
+    <Theme>
+      <App />
+    </Theme>
+  </ThemeProviderContext>
 );
