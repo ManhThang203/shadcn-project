a7ca5dffa08831e9588c8a8c725d2dcc1da9376b
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2026-01-04 21:03:15 +0700

    chore(deps): add @radix-ui/themes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
-// import { ThemeProvider as ThemeProviderContext } from "./context/ThemeProvider";
+import { ThemeProvider as ThemeProviderContext } from "./context/ThemeProvider";
 createRoot(document.getElementById("root")).render(
   <Theme>
     <App />
